dbc27a7ae189ed94eb18828a7efa56d22be4f644
EA-Digifolk <nadia.eudigifolk@gmail.com>
2024-11-06 21:38:45 +0100

    Title Statement Done! TODO: Checks on inputs

--- src/components/MeiConstructor.vue
+++ src/components/MeiConstructor.vue
@@ -2,20 +2,53 @@
   <div>
     <h1>MEI Constructor</h1>
 
-    <div class="container-lg mb-5 mt-5">
+    <div class="container-xxl mb-5 mt-5" v-if="MEIData === ''">
       <div class="row ">
         <!-- File Input for MEI, MusicXML and MIDI -->
-        <div class="col d-flex justify-content-start flex-wrap align-content-center border">
-          <label class="w-100" style="text-align: left">Load MEI or MusicXML</label> <!--or MIDI (,.mid)-->
-          <input class="w-100" type="file" @change="handleFiles" accept=".mei,.musicxml" />
+        <div class="col d-flex justify-content-start flex-wrap align-content-center border bg-light">
+          <label class="w-100 p-3" style="text-align: left">Load MEI or MusicXML</label> <!--or MIDI (,.mid)-->
+          <input class="w-100 p-3" type="file" @change="handleFiles" accept=".mei,.musicxml" />
         </div>
         <!-- Textarea for ABC String -->
-        <div class="col d-flex flex-wrap-wrap align-content-center border">
+        <div class="col d-flex flex-wrap-wrap align-content-center border bg-light">
           <textarea class="w-100" v-model="abcString" placeholder="Enter ABC string"></textarea>
         </div>
       </div>
-      <div class="row justify-content-center">         
+      <div class="row justify-content-center">
         <button class="w-100" @click="startProcess">Start</button>
+      </div>
+    </div>
+    <div class="container-xxl mb-5 mt-5" v-else>
+      <div id="carouselExampleIndicators" class="carousel carousel-dark slide w-100" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+            aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+            aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+            aria-label="Slide 3"></button>
+        </div>
+
+        <div class="carousel-inner">
+          <TitleStmtForm class="carousel-item active" :MEIData="xmlDoc"/>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+          </div>
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
 
@@ -25,14 +58,18 @@
 
 <script>
 import { ref } from 'vue';
-import { onMounted } from 'vue'
+import { onMounted } from 'vue';
 
 import createVerovioModule from 'verovio/wasm';
 import { VerovioToolkit } from 'verovio/esm';
 import * as music21 from 'music21j';
 
+import TitleStmtForm from './TitleStmtForm.vue';
 
 export default {
+  components: {
+    TitleStmtForm
+  },
   setup() {
     // Reactive references
     const MEIData = ref('');
@@ -41,6 +78,7 @@ export default {
     const abcString = ref('');
     const message = ref('');
     const verovioToolkit = ref('');
+    const xmlDoc = ref('');
 
     onMounted(() => {
       console.log(`the component is now mounted.`);
@@ -67,21 +105,25 @@ export default {
     };
 
     const startProcess = () => {
-      if (fileName.value.endsWith('.abc')) {
+      if (fileName.value.endsWith('.abc') || abcString.value !== '') {
         message.value = 'Loading ABC';
-        verovioToolkit.value.loadData(abcString);
+        verovioToolkit.value.loadData(abcString.value);
         MEIData.value = verovioToolkit.value.getMEI();
+        message.value = '';
       } else if (fileName.value.endsWith('.musicxml')) {
         message.value = 'Loading MusicXML';
         verovioToolkit.value.loadData(fileData.value);
         MEIData.value = verovioToolkit.value.getMEI();
+        message.value = '';
       } else if (fileName.value.endsWith('.mid')) {
         message.value = 'MIDI is not allowed for now';
         console.log(fileData.value);
         console.log(music21.converter.parseData(fileData.value));
+        message.value = '';
       } else if (fileName.value.endsWith('.mei')) {
         message.value = 'Loading MEI';
         MEIData.value = fileData.value;
+        message.value = '';
       } else {
         message.value = 'File type of ' + file.name + ' is not allowed at the moment!';
       };
@@ -100,10 +142,12 @@ export default {
       const n = new music21.note.Note('F#');
       */
 
+      xmlDoc.value = (new DOMParser()).parseFromString(MEIData.value, "text/xml");
     };
 
     // Return reactive references and methods
     return {
+      xmlDoc,
       MEIData,
       fileData,
       fileName,
---
+++ src/components/TitleStmtForm.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="card" style="width: 50em;">
+        <h4 class="card-header">
+            Title Statement Form
+        </h4>
+        <div class="card-body container">
+            <div id="form" class="mt-1 mb-3 pt-0 pb-0 p-5">
+                <li class="row mb-1" v-for="item in titleStmtData">
+                    <div class="col col-sm-2 card-text">
+                        <p class="card-text">{{ item.on_display }}</p>
+                    </div>
+                    <div class="col col-sm-10 card-text"> <input class="w-100 p-1" type="text" v-model="item.value"
+                            :placeholder="item.default" /> </div>
+                </li>
+            </div>
+            <button href="#" class="row btn btn-primary" @click="saveToMEI">Apply To MEI</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+
+export default {
+    props: ['MEIData'],
+    setup(props) {
+        const titleStmtData = ref([
+            { name: 'id', tag: './/mei:titleStmt//mei:title[@type="main"]', value: '', on_display: 'ID', default: 'CO-YEAR-RE-SUB-NUM' },
+            { name: 'title', tag: './/mei:titleStmt//mei:title', value: '', on_display: 'Title', default: '' },
+            { name: 'subtitle', tag: './/mei:titleStmt//mei:title[@type="subtitle"]', value: '', on_display: 'Subtitle', default: '' },
+            { name: 'composer', tag: './/mei:titleStmt//mei:respStmt//mei:persName[@role="composer"]', value: '', on_display: 'Composer', default: '' },
+            { name: 'compiler', tag: './/mei:titleStmt//mei:respStmt//mei:persName[@role="compiler"]', value: '', on_display: 'Compiler', default: '' },
+            { name: 'informer', tag: './/mei:titleStmt//mei:respStmt//mei:persName[@role="informer"]', value: '', on_display: 'Informer', default: '' },
+            { name: 'encoder', tag: './/mei:titleStmt//mei:respStmt//mei:persName[@role="encoder"]', value: '', on_display: 'Encoder', default: '' },
+            { name: 'editor', tag: './/mei:titleStmt//mei:respStmt//mei:persName[@role="editor"]', value: '', on_display: 'Editor', default: '' },
+            { name: 'geogName', tag: './/mei:titleStmt//mei:respStmt//mei:persName[@role="informer"]//mei:geogName', value: '', on_display: 'Geography', default: '' },
+        ]);
+
+        onMounted(() => {
+            console.log(`the component is now mounted.`);
+            //console.log(props.MEIData);
+            getInfoFromMEI();
+        });
+
+        const saveToMEI = () => {
+            for (let i in titleStmtData.value) {
+                let item = titleStmtData.value[i];
+                let node = getXpathNode(props.MEIData, item.tag);
+
+                if (!node) {
+                    console.log('No node with tag: ' + item.tag);
+                    if (item.name == 'id') {
+                        let nodeT = getXpathNode(props.MEIData, titleStmtData.value[1].tag);
+                        if (!nodeT.hasAttribute('type')) {
+                            nodeT.setAttribute('type', "main");
+                        }; node = nodeT;
+                    } else if (item.name == 'subtitle') {
+                        let nodeT = getXpathNode(props.MEIData, titleStmtData.value[1].tag);
+                        let node = document.createElementNS('http://www.music-encoding.org/ns/mei','title');
+                        node.setAttribute('type', 'subtitle');
+                        nodeT.insertAdjacentElement("afterend", node);
+                    } else if (item.name == 'geogName') {
+                        console.log(props.MEIData);
+                        let nodeR = getXpathNode(props.MEIData, titleStmtData.value[8].tag);
+                        let node = document.createElementNS('http://www.music-encoding.org/ns/mei','geogName');
+                        nodeR.append(node);
+                    } else {
+                        let nodeR = getXpathNode(props.MEIData, './/mei:titleStmt//mei:respStmt');
+                        let node = document.createElementNS('http://www.music-encoding.org/ns/mei','persName');
+                        node.setAttribute('role', item.name );
+                        nodeR.append(node);
+                    }
+                }
+
+                if (node) {
+                    if (item.name == 'id') {
+                        node.setAttribute('xml:id', item.value)
+                    } else if (item.name == 'informer') {
+                        let tempChildren = node.children[0]
+                        node.textContent = item.value;
+                        node.append(tempChildren);
+                    } else {
+                        node.textContent = item.value;
+                    }
+                }
+            }
+
+            console.log(props.MEIData)
+        };
+
+        const getXpathNode = (nodeP, xpath) => {
+            const result = nodeP.evaluate(xpath, nodeP, prefix => prefix === 'mei' ? 'http://www.music-encoding.org/ns/mei' : null, XPathResult.ANY_TYPE, null);
+            return result.iterateNext();
+        }
+
+        const getInfoFromMEI = () => {
+
+            for (let i in titleStmtData.value) {
+                let item = titleStmtData.value[i];
+                let node = getXpathNode(props.MEIData, item.tag);
+                if (node) {
+                    if (item.name == 'id') {
+                        item.value = node.getAttribute('xml:id')
+                    } else {
+                        item.value = node.textContent;
+                    }
+                }
+            }
+        };
+
+        return {
+            titleStmtData,
+            getXpathNode,
+            getInfoFromMEI,
+            saveToMEI,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
